8bd8253ebc24530e86549c449e69195fcf4005bc
Sergey Kudashev <sergey.m.kudashev@gmail.com>
2025-04-05 23:58:30 +0300

    fix index.js

--- backend/index.js
+++ backend/index.js
@@ -13,16 +13,17 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-const allowedOrigins = [
-    process.env.FRONTEND_URL,           // продакшн фронт
-    'http://localhost:5173'             // локальный фронт (Vite)
-  ];
-  
-  app.use(cors({
-    origin: allowedOrigins,
+// const allowedOrigins = [
+//     process.env.FRONTEND_URL,           // продакшн фронт
+//     'http://localhost:5173'             // локальный фронт (Vite)
+//   ];
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
-  }));
+}));
+
 app.use(cookieParser());
 app.use(express.static('public'));
 
@@ -49,7 +50,7 @@ const verifyUser = (req, res, next) => {
 }
 
 app.get("/", verifyUser, (req, res) => {
-    return res.json({email: req.email, username: req.username})
+    return res.json({ email: req.email, username: req.username })
 })
 
 const storage = multer.diskStorage({
@@ -70,54 +71,54 @@ app.post("/create", verifyUser, upload.single("file"), (req, res) => {
     PostModel.create({
         title: req.body.title,
         description: req.body.description,
-        file: req.file.filename, 
+        file: req.file.filename,
         email: req.body.email,
         username: req.body.username
     })
-    .then(result => res.json("Success"))
-    .catch(err => res.json(err))
+        .then(result => res.json("Success"))
+        .catch(err => res.json(err))
 })
 
 app.post("/register", (req, res) => {
-    const {username, email, password} = req.body;
+    const { username, email, password } = req.body;
     bcrypt.hash(password, 10)
-    .then(hash => {
-        // потом изменить approved на false
-        UserModel.create({username, email, password: hash, approved: true })
-        .then(user => res.json(user))
-        .catch(err => res.json(err))
-    }).catch(err => res.json(err))
+        .then(hash => {
+            // потом изменить approved на false
+            UserModel.create({ username, email, password: hash, approved: true })
+                .then(user => res.json(user))
+                .catch(err => res.json(err))
+        }).catch(err => res.json(err))
 })
 
 app.post("/login", (req, res) => {
-    const {email, password} = req.body;
-    UserModel.findOne({email: email})
-    .then(user => {
-        if (!user) {
-            return res.json("User not exist")
-        }
-        else if (user && user.approved) {
-            bcrypt.compare(password, user.password, (err, response) => {
-                if (response) {
-                    const token = jwt.sign({email: user.email, username: user.username},
-                    "jwt-secret-key",
-                    {expiresIn: "1d"})
-                    res.cookie("token", token, {
-                        httpOnly: true,
-                        secure: true,
-                        sameSite: "None"
-                      });
-                    return res.json("Success")
-                }
-                else {
-                    return res.json("Incorrect Password")
-                }
-            })
-        }
-        else {
-            return res.json("User is not approved")
-        }
-    })
+    const { email, password } = req.body;
+    UserModel.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                return res.json("User not exist")
+            }
+            else if (user && user.approved) {
+                bcrypt.compare(password, user.password, (err, response) => {
+                    if (response) {
+                        const token = jwt.sign({ email: user.email, username: user.username },
+                            "jwt-secret-key",
+                            { expiresIn: "1d" })
+                        res.cookie("token", token, {
+                            httpOnly: true,
+                            secure: true,
+                            sameSite: "None"
+                        });
+                        return res.json("Success")
+                    }
+                    else {
+                        return res.json("Incorrect Password")
+                    }
+                })
+            }
+            else {
+                return res.json("User is not approved")
+            }
+        })
 })
 
 // app.get("/logout", (req, res) => {
@@ -127,41 +128,41 @@ app.post("/login", (req, res) => {
 
 app.get("/logout", (req, res) => {
     res.clearCookie("token", {
-      httpOnly: true,
-      secure: true,
-      sameSite: "None"
+        httpOnly: true,
+        secure: true,
+        sameSite: "None"
     });
     return res.json("Success");
-  });
+});
 
 app.get('/getposts', (req, res) => {
     PostModel.find()
-    .then(posts => res.json(posts))
-    .catch(err => res.json(err))
+        .then(posts => res.json(posts))
+        .catch(err => res.json(err))
 })
 
 app.get('/getpostbyid/:id', (req, res) => {
     const id = req.params.id;
-    PostModel.findById({_id: id})
-    .then(post => res.json(post))
-    .catch(err => res.json(err))
+    PostModel.findById({ _id: id })
+        .then(post => res.json(post))
+        .catch(err => res.json(err))
 })
 
 app.delete('/deletepostbyid/:id', (req, res) => {
     const id = req.params.id;
-    PostModel.findByIdAndDelete({_id: id})
-    .then(post => res.json(post))
-    .catch(err => res.json(err))
+    PostModel.findByIdAndDelete({ _id: id })
+        .then(post => res.json(post))
+        .catch(err => res.json(err))
 })
 
 app.put('/editpostbyid/:id', (req, res) => {
     const id = req.params.id;
-    PostModel.findByIdAndUpdate({_id: id}, {title: req.body.title, description: req.body.description})
-    .then(post => res.json("Success"))
-    .catch(err => res.json(err))
+    PostModel.findByIdAndUpdate({ _id: id }, { title: req.body.title, description: req.body.description })
+        .then(post => res.json("Success"))
+        .catch(err => res.json(err))
 
 })
 
-app.listen(3001, function() {
+app.listen(3001, function () {
     console.log('listening on port 3001');
 })
